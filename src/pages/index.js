b6e13ebdc526ac4939c9f498eaff7b7208479d88
Waliyah-O <waliyah.okunade@i-academy.org>
2023-11-28 15:47:01 +0100

    route

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const index = () => {
     <div>
       <h1>Dashboard</h1>
       <Link to={'./signup'}>Signup</Link> <br />
-      <Link to={'./signup.completeprofile'}>complete profile</Link> <br />
+      <Link to={'./completeprofile'}>complete profile</Link> <br />
       <Link to={'./signin'}>Signin</Link> <br />
       <Link to={'./forgotpassword'}>forgot password</Link> <br />
       <Link to={'./passwordreset'}>Reset Password</Link>
