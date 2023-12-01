832ae16bc877df7cbc0d6b3a9f39f2a21d65c739
Waliyah-O <waliyah.okunade@i-academy.org>
2023-12-01 16:56:09 +0100

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
 import { Link } from 'react-router-dom';
-import CustomCarousel from '../components/carousel/CustomCarousel';
+import UseCase from '../components/pagination/useCase'
 
 const Index = () => {
-  //for CustomCarousel
-  const carouselLinks = [
-    { id: 1, url: 'https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg' },
-    { id: 2, url: 'https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg' },
-    { id: 3, url: 'https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg' },
-    { id: 4, url: 'https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg' },
-  ];
 
   return (
     <div>
