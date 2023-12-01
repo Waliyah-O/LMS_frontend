4d46cc43240e7fc44a42eaa05871fde9906d4171
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-12-01 08:51:52 +0100

    customCarousel added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import CheckBox from '../components/customInputs/CheckBox';
 import RadioButton from '../components/customInputs/RadioButton';
 import TextArea from '../components/customInputs/TextArea';
+// import CustomCarousel from '../components/carousel/CustomCarousel';
 
 const index = () => {
   const countryOptions = [
@@ -10,6 +11,15 @@ const index = () => {
     { position: 3, label: 'Egypt' },
     { position: 4, label: 'Togo' },
   ];
+
+  //for CustomCarousel
+  // const carouselLinks = [
+  //   { id: 1, url: 'https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg' },
+  //   { id: 2, url: 'https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg' },
+  //   { id: 3, url: 'https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg' },
+  //   { id: 4, url: 'https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg' },
+  // ];
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -20,7 +30,8 @@ const index = () => {
       <Link to={'./passwordreset'}>Reset Password</Link>
       <CheckBox options={countryOptions} labelText={'Check Box'} className={'checkbox-success'} />
       <RadioButton options={countryOptions} className={'radio-success checked:bg-green-700'} labelText={'Pick one'} />
-      <TextArea labelText={'Enter Message'} placeholder={'Enter text....'}/>
+      <TextArea labelText={'Enter Message'} placeholder={'Enter text....'} />
+      {/* <CustomCarousel carouselImgSrc={carouselLinks} /> */}
     </div>
   );
 };
