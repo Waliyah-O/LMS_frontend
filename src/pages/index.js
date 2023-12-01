3a156d4d17a38ab58fced378bfaae12a78a49c5c
Waliyah-O <waliyah.okunade@i-academy.org>
2023-12-01 16:54:32 +0100

    table

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom';
-import UseCase from '../components/pagination/useCase';
-// import CustomCarousel from '../components/carousel/CustomCarousel';
+import CustomCarousel from '../components/carousel/CustomCarousel';
 
 const Index = () => {
 
 
   //for CustomCarousel
-  // const carouselLinks = [
-  //   { id: 1, url: 'https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg' },
-  //   { id: 2, url: 'https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg' },
-  //   { id: 3, url: 'https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg' },
-  //   { id: 4, url: 'https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg' },
-  // ];
+  const carouselLinks = [
+    { id: 1, url: 'https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg' },
+    { id: 2, url: 'https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg' },
+    { id: 3, url: 'https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg' },
+    { id: 4, url: 'https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg' },
+  ];
 
   return (
     <div>
@@ -21,7 +20,7 @@ const Index = () => {
       <Link to={'./signin'}>Signin</Link> <br />
       <Link to={'./forgotpassword'}>forgot password</Link> <br />
       <Link to={'./passwordreset'}>Reset Password</Link>
-      <UseCase/>
+      <CustomCarousel/>
     </div>
   );
 };
