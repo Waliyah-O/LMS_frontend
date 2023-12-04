9c8bef5f548ff6c0911a3f28220f0b677b805ef3
MrPhilus <theooophii@gmail.com>
2023-12-04 16:29:34 +0100

    redudant dependency removed

--- src/components/alerts/alertTypes.js
+++ src/components/alerts/alertTypes.js
@@ -1,4 +1,4 @@
-export const defaultAlert = () => {
+export const infoAlert = () => {
   return 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z';
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import UseCase from '../components/pagination/useCase';
+import Alerts from '../components/alerts/Alerts';
+import { types } from '../components/alerts';
 // import CustomCarousel from '../components/carousel/CustomCarousel';
 
 const Index = () => {
@@ -20,6 +22,7 @@ const Index = () => {
       <Link to={'./forgotpassword'}>forgot password</Link> <br />
       <Link to={'./passwordreset'}>Reset Password</Link>
       <UseCase />
+      <Alerts alertStyle="alert-info" alertType={types.infoAlert()} alertText="This is an alert" />
     </div>
   );
 };
