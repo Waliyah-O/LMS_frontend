690ac5e8c520f4d21f8afffd293833fbc5d43edd
Waliyah-O <waliyah.okunade@i-academy.org>
2023-12-06 10:04:15 +0100

    features

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,49 +1,9 @@
-const Card = ({children}) => {
+const Card = ({ children }) => {
   return (
     <div>
       <div className="">
-        <div className="relative group cursor-pointer group overflow-hidden text-gray-50 h-72 w-full  rounded-2xl hover:duration-700 duration-700">
-          <div className="w-full h-72 bg-lime-400 text-gray-800">
-            <div className="flex flex-row justify-between">
-              <svg
-                className="fill-current stroke-current w-8 h-8 p-2 hover:bg-lime-200  rounded-full m-1"
-                height="100"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 100 100"
-                width="100"
-                x="0"
-                xmlns="http://www.w3.org/2000/svg"
-                y="0"
-              >
-                <path
-                  className=""
-                  d="M15.8,32.9V15.8m0,0H32.9m-17.1,0L37.2,37.2m47-4.3V15.8m0,0H67.1m17.1,0L62.8,37.2m-47,29.9V84.2m0,0H32.9m-17.1,0L37.2,62.8m47,21.4L62.8,62.8M84.2,84.2V67.1m0,17.1H67.1"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="8"
-                ></path>
-              </svg>
-              <svg
-                className="fill-current stroke-current w-8 h-8 p-2 m-1 hover:bg-lime-200 rounded-full"
-                height="100"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 100 100"
-                width="100"
-                x="0"
-                xmlns="http://www.w3.org/2000/svg"
-                y="0"
-              >
-                <path
-                  className="svg-stroke-primary"
-                  d="M50,17.4h0M50,50h0m0,32.6h0M50,22a4.7,4.7,0,1,1,4.7-4.6A4.7,4.7,0,0,1,50,22Zm0,32.7A4.7,4.7,0,1,1,54.7,50,4.7,4.7,0,0,1,50,54.7Zm0,32.6a4.7,4.7,0,1,1,4.7-4.7A4.7,4.7,0,0,1,50,87.3Z"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="8"
-                ></path>
-              </svg>
-            </div>
+        <div className="relative group cursor-pointer group overflow-hidden text-gray-50 h-72 w-full  rounded-2xl hover:duration-700 duration-700 gap-2">
+          <div className="w-full h-72">
           </div>
           {children}
         </div>
