571466f0865955e9d05e272e2f3bacf8a9083792
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-12-06 11:27:00 +0100

    BFooter Updated

---
+++ src/components/customInputs/listComponent/ListComponent.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import LogoBullets from '../../../assets/images/LogoBullets.svg';
+
+const ListComponent = () => {
+  return (
+    <div>
+      <ul className="font-inter text-md leading-[3.3rem] text-white">
+        <li>Centralized organization</li>
+        <li>Enhanced accessibility</li>
+        {/* <img src={LogoB} /> */}
+        <li>Improved communication</li>
+        <li>Streamlined grading and feedback</li>
+        <li>Centralized organization</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ListComponent;
